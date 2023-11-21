b6a79b563670f6e8b136cd815257c25b76566939
Marc Vautheny <marc@vautheny.com>
2023-11-21 10:23:36 +0300

    added base components and some css

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-center'><a className='p-5' href='#about-me'>About Me</a><a className='p-5' href='#projects'>Projects</a></div>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <AboutMe />
+      <Projects />
     </main>
   )
 }
