e78abc0507c68b5bb87b1055f7ccdc67bb887a3c
Marc Vautheny <marc@vautheny.com>
2023-11-22 10:39:51 +0300

    changed product card content changed Projects hardcoded data added some product card css and image

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 import React from "react";
-
-const ProjectCard = () => {
+import stock_image from '@/public/images/Nitro_Wallpaper_5000x2813.jpg';
+import Image from "next/image";
+const ProjectCard = ({ project }) => {
   return (
     <div className="p-4">
-      <Link href='/'>
-        <h3>Project Name</h3>
-        <p>
-          Project description. Tech stack, learning acheived, challenges, redos,
-          skill developed.
-        </p>
-        <p>Image</p>
+      <Link href="/">
+        <h3>{project.name}</h3>
+        <Image src={stock_image} alt='stock image for project image' />
+        <p>{project.description}</p>
+        <div>{project.tags.join(', ')}</div>
       </Link>
     </div>
   );
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,13 +5,15 @@ const Projects = () => {
   const Projects = [
       {
         name: "React - ONS Data Website Interface",
+        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
         tags: ["React", "Third-Party API", "JS"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
       {
         name: "Python - Hangman Game",
-        front_end: ["Python", "Firebase"],
+        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
+        tags: ["Python", "Firebase"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
@@ -21,8 +23,8 @@ const Projects = () => {
     <div id="projects">
       <h2 className="w-fill text-center pb-5">Projects</h2>
       <div className="grid grid-cols-3 grid-rows-2 grid-flow-row-dense">
-        {Projects.map(()=>{
-            return (<ProjectCard />)
+        {Projects.map((project)=>{
+            return (<ProjectCard project={project}/>)
         })}
       </div>
     </div>
