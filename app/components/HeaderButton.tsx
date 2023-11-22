3ac9b0c48e4b0b16652e3f94070ef48caced7b67
Marc Vautheny <marc@vautheny.com>
2023-11-22 11:21:01 +0300

    changed nav button css

--- app/components/HeaderButton.tsx
+++ app/components/HeaderButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeaderButton = ( {children, href}) => {
   return (
-    <a className="p-5 hover:bg-slate-300 rounded-l" href={href}>
+    <a className="p-5 hover:bg-slate-200 rounded-xl font-bold m-1" href={href}>
       {children}
     </a>
   );
