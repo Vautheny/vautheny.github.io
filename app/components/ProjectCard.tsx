69257e084230b1fdf0a28a75dfad8dd48c776e8f
Marc Vautheny <marc@vautheny.com>
2023-11-23 11:54:02 +0300

    changed field name and added more project details

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ project }) => {
     <Link href="/" className="border-solid border-1 rounded-xl p-3 m-1 shadow-xl hover:shadow-2xl hover:border-gray-200 hover:border-2">
       <h3>{project.name}</h3>
       <Image src={stock_image} alt="stock image for project image" className="rounded-xl" />
-      <p>{project.description}</p>
+      <p>{project.short_description}</p>
       <ProjectTagContainer tags={project.tags} />
     </Link>
   );
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,48 +5,46 @@ const Projects = () => {
   const Projects = [
       {
         name: "React - ONS Data Website Interface",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
+        short_description: 'Assessment porject for Army coding course. Task required connection and use of public APIs. Site needed to allow users to select what data they wanted to view.',
         tags: ["React", "Third-Party API", "JS"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
       {
         name: "Python - Hangman Game",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
+        short_description: 'Assessment for Army JHUB Module 3 course. Required the simple hangman game to be played in shell using Python.',
         tags: ["Python", "Firebase"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },  
       {
         name: "React - Tic Tac Toe Game",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
+        short_description: 'Self assigned project for working with React and basic deployabilty options. Used Firebase due to course reccomendation but also ease of deployment.',
         tags: ["React", "Firebase"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
       {
-        name: "React - Tic Tac Toe Game",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
+        name: "React - Sailing Logbook",
+        short_description: 'Self assigned project logging sailing trips. A simpler alternative to publicly available apps on the market.',
         tags: ["React", "Firebase"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
       {
-        name: "React - Tic Tac Toe Game",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
-        tags: ["React", "Firebase"],
+        name: "React - RLC Association Funding App",
+        short_description: 'Production level project for the RLC Association. Required app to reduce current workload of paper based applications.',
+        tags: ["React",, "Node.js", "Azure", "Firebase", "AWS", "Nodemailer", "MYSQL"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
       },
       {
-        name: "React - Tic Tac Toe Game",
-        description: 'Project description. Tech stack, learning acheived, challenges, redos, skill developed.',
-        tags: ["React", "Firebase"],
+        name: "NextJS - Software Development Portfolio",
+        short_description: 'This website. A demonstration of coding ability and a location to advertise experience and knowledge.',
+        tags: ["NextJS", "Prisma", "Tailwind CSS", "DaisyUI", "MYSQL"],
         github: "https://www.githublink.com",
         demo: "https://www.demosite.com"
-      },
-
-      
+      },      
   ];
 
   return (
