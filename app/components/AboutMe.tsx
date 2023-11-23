4c4e9a636ff695102b2384c44f932f5d4ba63787
Marc Vautheny <marc@vautheny.com>
2023-11-23 16:50:36 +0300

    filled about me page and p tag css

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -2,11 +2,36 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div id="about-me" className="pb-10">
+    <div id="about-me" className="pb-10 max-w-4xl">
       <h2>About Me</h2>
       <p>
-        Some text about me. What my goals are, strengths, value-add, weaknesses
-        and development goals.
+        I'm an Animal Scientist gone Accountant gone Army Soldier, with various
+        interludes inbetween. My LinkedIn profile is best for all that kind of
+        detail though. Whilst wearing all these hats I've coded as a hobby since
+        secondary with varying degrees of commitment and proficiency. My guiding
+        project has always been an all encompassing personal finance website.
+        One of the problems I encountered was as I delved in and out, new tools
+        and methods would appear and the project would start from scatch, going
+        from one framework and language to the next. This has not been a totally
+        bad thing as I've been able to learn more about what has now become my
+        trade.
+      </p>
+      <p>
+        Although never in a technical role, I would focus on those aspects that
+        were. The world of excel accounting was a prime example, which as a
+        result lead me to learn VBA. Even as a Port Operative in the army, my
+        skills in IT meant I would end up completing their JHUB coding scheme,
+        learning Python and React. I was even trawled to produce a web app for
+        the RLC Association and attend the 2023 Defence Hackathon.
+      </p>
+      <p>
+        My interest lay with data manipulation and presentation, but any
+        challenge that allows exposure to the fullstack is always preferred.
+      </p>
+      <p>
+        Current aspirations are to seek developer roles within the MoD. If no
+        suitable roles are found or I'm unable to qualify for them then civy
+        alternatives will be the goal.
       </p>
     </div>
   );
