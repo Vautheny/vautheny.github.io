c472affac7f8472e7db45dab3fcadcdfb04bbaa5
Marc Vautheny <marc@vautheny.com>
2023-11-23 20:54:54 +0300

    added onclick bgColor change

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
 
-const Skill = ({skill}) => {
-  return (
-    <div className='bg-slate-100 flex p-2 m-2 rounded-xl'>{skill}</div>
-  )
-}
+import React, { useState } from "react";
 
-export default Skill
+const Skill = ({ skill }) => {
+  const [bgColor, setBgColor] = useState('bg-slate-100')
+  const changeBgColorHandler = (event) => {
+    bgColor == 'bg-slate-100' && setBgColor('bg-blue-200');
+    bgColor == 'bg-blue-200' && setBgColor('bg-slate-100');
+  }
+  return <div className={`${bgColor} flex p-2 m-2 rounded-xl`} onClick={changeBgColorHandler}>{skill}</div>;
+};
+
+export default Skill;
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import Skill from './Skill'
+import React from "react";
+import Skill from "./Skill";
 
 const Skills = () => {
-    const skills = [
-        'React',
-        'NextJS',
-        'Tailwind CSS',
-        'DaisyUI',
-        'Bootstrap CSS',
-        'Firebase',
-        'Azure',
-        'AWS',
-        'Python',
-        'Django',
-        'PHP',
-        'JavaScript',
-        'VBA',
-        'Heroku',
-        'NodeJS',
-        'MYSQL',
-        'Prisma',
-        'Nodemailer',
-        'Third-Party API'
-    ]
-  return (
-    <div className='flex max-w-4xl flex-wrap m-5'>
-        <h2>Skills</h2>
-        {skills.map((skill)=>{ return <Skill skill={skill} />})}
-    </div>
-  )
-}
 
-export default Skills
+
+  const skills = [
+    "React",
+    "NextJS",
+    "Tailwind CSS",
+    "DaisyUI",
+    "Bootstrap CSS",
+    "Firebase",
+    "Azure",
+    "AWS",
+    "Python",
+    "Django",
+    "PHP",
+    "JavaScript",
+    "VBA",
+    "Heroku",
+    "NodeJS",
+    "MYSQL",
+    "Prisma",
+    "Nodemailer",
+    "Third-Party API",
+  ];
+  return (
+    <div className="flex max-w-4xl flex-wrap m-5">
+      <h2>Skills</h2>
+      {skills.map((skill) => {
+        return <Skill skill={skill} />;
+      })}
+    </div>
+  );
+};
+
+export default Skills;
