068e88f0791189be901a867e541715ce96bfb39c
Marc Vautheny <marc@vautheny.com>
2023-12-03 16:44:48 +0200

    improved about me text

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -3,35 +3,45 @@ import React from "react";
 const AboutMe = () => {
   return (
     <div id="about-me" className="m-5 max-w-4xl">
-      <h2>About Me</h2>
+      <h2>Marc Vautheny</h2>
+      <p>Welcome to my portfolio site.</p>
       <p>
-        I'm an Animal Scientist gone Accountant gone Army Soldier, with various
-        interludes inbetween. My LinkedIn profile is best for all that kind of
-        detail though. Whilst wearing all these hats I've coded as a hobby since
-        secondary with varying degrees of commitment and proficiency. My guiding
-        project has always been an all encompassing personal finance website.
-        One of the problems I encountered was as I delved in and out, new tools
-        and methods would appear and the project would start from scatch, going
-        from one framework and language to the next. This has not been a totally
-        bad thing as I've been able to learn more about what has now become my
-        trade.
+        A site to display my developer experience and be a central source
+        for all my professional online presence.
       </p>
       <p>
-        Although never in a technical role, I would focus on those aspects that
-        were. The world of excel accounting was a prime example, which as a
-        result lead me to learn VBA. Even as a Port Operative in the army, my
-        skills in IT meant I would end up completing their JHUB coding scheme,
-        learning Python and React. I was even trawled to produce a web app for
-        the RLC Association and attend the 2023 Defence Hackathon.
+        Historically I've been an Animal Scientist, Accountant, British Soldier,
+        and other various other interludes inbetween. LinkedIn is a better
+        source for all that detail.
       </p>
       <p>
-        My interest lay with data manipulation and presentation, but any
-        challenge that allows exposure to the fullstack is always preferred.
+        I've coded as a hobby since secondary (late teens), with varying degrees
+        of commitment and proficiency. Finances have also always been an
+        interest and as a result has been the main source of my earlier project
+        ideas. One problem I encountered was by delving in and out of coding,
+        new tools and methods would appear and the projects would start from
+        scatch, going from one framework and language to the next. This has not
+        been entirely bad as I've been able to learn more about what has now
+        become my trade. One con, is most of my earlier work as disappeared into
+        the ether or not of any quality to display here.
       </p>
       <p>
-        Current aspirations are to seek developer roles within the MoD. If no
-        suitable roles are found or I'm unable to qualify for them then civy
-        alternatives will be the goal.
+        My commitment to this tade of software developing finally occured during
+        my stint in the British Army. There I discovered and completed most of
+        the JHUB Coding Scheme, participated in the 2023 Defence Hackathon and
+        developed an in-house web app. This allowed me to learn modern coding
+        practices and tools that were valued in the industry.
+      </p>
+      <p>
+        What I love about the work; is the simple act of being able to create
+        something useful that I can share with others. It involves both problem
+        thinkning and creativity. Can be done anywhere, anytime, offline or
+        online (preferred!).
+      </p>
+      <p>
+        Current aspirations, as of Dec 2023, are to seek developer roles within
+        the MoD. If there aren't any suitable roles or I'm unable to qualify for
+        them, then I'll look for civilian jobs.
       </p>
     </div>
   );
