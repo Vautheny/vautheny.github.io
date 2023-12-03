a4863a649b83aa0b2d44b205f68b0ea2627dc8b4
Marc Vautheny <marc@vautheny.com>
2023-12-03 16:51:10 +0200

    added h1 header for seo

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "./components/Skills";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-10 pb-24 px-24">
+      <h1>Developer Portfolio</h1>
       <AboutMe />
       {/* <Skills /> */}
       <Projects />
